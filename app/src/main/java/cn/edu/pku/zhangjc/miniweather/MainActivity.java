8d590db9731487ff4eb8c5b41fbd16d12b4f362a
zjc <jinchuan209@163.com>
2018-09-27 00:29:12 +0800

    my first commit

---
+++ app/src/main/java/cn/edu/pku/zhangjc/miniweather/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.pku.zhangjc.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
