b7964d972fac35298269766cb8f5c26b7c66a1dd
zjc <jinchuan209@163.com>
2018-10-17 00:07:12 +0800

    weather09_v2已完成

--- app/src/main/java/db/CityDb.java
+++ app/src/main/java/db/CityDb.java
@@ -22,12 +22,12 @@ public class CityDb {
         List<City> list = new ArrayList<>();
         Cursor c = db.rawQuery("SELECT * from " + CITY_TABLE_NAME, null);
         while (c.moveToNext()) {
-            String province = c.getString(c.getColumnIndex("prov ince"));
+            String province = c.getString(c.getColumnIndex("province"));
             String city = c.getString(c.getColumnIndex("city"));
-            String number = c.getString(c.getColumnIndex("number "));
+            String number = c.getString(c.getColumnIndex("number"));
             String allPY = c.getString(c.getColumnIndex("allpy"));
-            String allFirstPY = c.getString(c.getColumnIndex("al lfirstpy"));
-            String firstPY = c.getString(c.getColumnIndex("first py"));
+            String allFirstPY = c.getString(c.getColumnIndex("allfirstpy"));
+            String firstPY = c.getString(c.getColumnIndex("firstpy"));
             City item = new City(province, city, number, firstPY, allPY, allFirstPY);
             list.add(item);
         }
