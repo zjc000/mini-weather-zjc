4db01d9a3250b03ba5fcd6ecfc55187084a8fe6e
zjc <jinchuan209@163.com>
2018-10-23 21:43:04 +0800

    weather11已完成

--- app/src/main/java/cn/edu/pku/zhangjc/miniweather/SelectCity.java
+++ app/src/main/java/cn/edu/pku/zhangjc/miniweather/SelectCity.java
@@ -5,18 +5,58 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.pku.zhangjc.app.MyApplication;
+import cn.edu.pku.zhangjc.bean.City;
 
 public class SelectCity extends Activity implements View.OnClickListener {
     private ImageView mBackBtn;
+    private ListView mList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_city);
+        initViews();
 
         mBackBtn = (ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
+    }
+
+    public void initViews() {
+        mBackBtn = (ImageView) findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
+        MyApplication myApplication = (MyApplication) getApplication();
+        List<City> cityList = myApplication.getCityList();
+        final List<String> cityName = new ArrayList<>();
+        final List<String> cityCodes = new ArrayList<>();
+        for (City city:cityList) {
+            cityCodes.add(city.getNumber());
+        }
+        for (City city:cityList) {
+            cityName.add(city.getCity());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SelectCity.this, android.R.layout.simple_list_item_1, cityName);
+        ListView listView = (ListView) findViewById(R.id.title_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //position 点击的Item位置，从0开始算
+                String cityCode = cityCodes.get(position);
+                Intent intent=new Intent();
+                intent.putExtra("cityCode",cityCode);//传递给下一个Activity的值
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 
     public void onClick(View v) {
